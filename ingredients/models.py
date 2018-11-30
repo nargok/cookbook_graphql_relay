649e4e42678a318a05d56cc21f6f072f1ae29da6
nargok <nargok@gmail.com>
2018-11-30 22:02:56 +0900

    migratoinを実行

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
   name = models.CharField(max_length=100)
 
@@ -10,7 +9,7 @@ class Category(models.Model):
 class Ingredient(models.Model):
   name = models.CharField(max_length=100)
   notes = models.TextField()
-  category = models.ForeignKey(Category, related_name='ingredients')
+  category = models.ForeignKey(Category, related_name='ingredients', on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
