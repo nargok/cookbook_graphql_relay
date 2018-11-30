21f83a28fd74256d9ddb4b575f0ed4e06826e7c2
nargok <nargok@gmail.com>
2018-11-30 22:39:46 +0900

    Schemaとurlの設定

--- cookbook_relay/settings.py
+++ cookbook_relay/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ingredients'
+    'graphene_django',
+    'ingredients',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'cookbook_relay.schema.schema'
+}
